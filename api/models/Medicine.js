a08df73291995fcb1526507f94bedd4f6e59edc1
hanswatkins <hanswatkins@gmail.com>
2023-05-05 13:37:30 -0700

    minor changes to Schemas were reformatted

--- api/models/Medicine.js
+++ api/models/Medicine.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const MedicineSchema = new Schema({
-	title: String,
-	instructions: String,
-	notes: String,
-	time: [String],
-	type: String,
-	patient: { type: Schema.Types.ObjectId, ref: 'User' },
-},
-{
-	timestamps: true
-});
+const MedicineSchema = new Schema(
+	{
+		title: String,
+		instructions: String,
+		notes: String,
+		time: [String],
+		type: String,
+		patient: { type: Schema.Types.ObjectId, ref: 'User' },
+	},
+	{
+		timestamps: true,
+	}
+);
 
 const MedicineModel = model('Medicine', MedicineSchema);
 
